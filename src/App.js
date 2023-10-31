905cd1dfe3ee508bf57df252a6499866907c82e8
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-10-31 10:36:41 +0530

    Add code to update the color

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import MainBox from './components/MainBox';
 
 const App = () => {
+  const [color, setColor] = useState('purple')
+
+  const handleColorChange = (newColor) => {
+    setColor(newColor);
+  };
 
   return (
     <>
-    <MainBox color={'some'} />
-    <h1> Select color</h1>
-    <div style={{display: 'flex'}}>
-      <div style={{backgroundColor: 'red', height: 50, width: 50}}></div>
-      <div style={{backgroundColor: 'yellow', height: 50, width: 50}}></div>
-      <div style={{backgroundColor: 'green', height: 50, width: 50}}></div>
-      <div style={{backgroundColor: 'blue', height: 50, width: 50}}></div>
-      <div style={{backgroundColor: 'orange', height: 50, width: 50}}></div>
-      <div style={{backgroundColor: 'black', height: 50, width: 50}}></div>
-    </div>
+      <MainBox color={color} />
+      <h1> Select color </h1>
+      <div style={{display: 'flex'}}>
+      <div
+          style={{ backgroundColor: 'red', height: 50, width: 50 }}
+          onClick={() => handleColorChange('red')}
+        ></div>
+        <div
+          style={{ backgroundColor: 'yellow', height: 50, width: 50 }}
+          onClick={() => handleColorChange('yellow')}
+        ></div>
+        <div
+          style={{ backgroundColor: 'green', height: 50, width: 50 }}
+          onClick={() => handleColorChange('green')}
+        ></div>
+        <div
+          style={{ backgroundColor: 'blue', height: 50, width: 50 }}
+          onClick={() => handleColorChange('blue')}
+        ></div>
+        <div
+          style={{ backgroundColor: 'orange', height: 50, width: 50 }}
+          onClick={() => handleColorChange('orange')}
+        ></div>
+        <div
+          style={{ backgroundColor: 'black', height: 50, width: 50 }}
+          onClick={() => handleColorChange('black')}
+        ></div>
+      </div>
     </>
   );
   
--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 function MainBox({ color }) {
   return <div
       style={{
-        backgroundColor: 'blue',
+        backgroundColor: color,
         width: '500px',
         height: '500px'
       }}
